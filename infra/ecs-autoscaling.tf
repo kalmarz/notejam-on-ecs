6fb1516ba29696547fe38d4214c137ae529114ee
Zoltan Kalmar <zoltan.kalmar@pm.me>
2020-11-26 12:07:05 +0100

    - ecs autoscaling completed

--- infra/ecs-autoscaling.tf
+++ infra/ecs-autoscaling.tf
@@ -1,10 +1,25 @@
-/*
 resource "aws_appautoscaling_target" "ecs_target" {
   max_capacity       = 4
   min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.example.name}/${aws_ecs_service.example.name}"
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.notejam-ecs-svc.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
-*/
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "${var.APP}-${var.ENV}-ecs-autoscaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 60
+    scale_in_cooldown  = 5
+    scale_out_cooldown = 5
+  }
+}
